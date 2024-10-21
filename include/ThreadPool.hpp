ce3782dcf1d6b65d5cf22f801fdc57f2a257ea42
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2024-10-21 21:35:10 +0800

    success

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -10,41 +10,85 @@
 #include <vector>
 #include <memory>
 #include <thread>
+#include <future>
 #include <functional>
+#include <iostream>
+#include <unordered_map>
 #include <condition_variable>
 
 class ThreadPool {
 public:
-    ThreadPool() = default;
-    explicit ThreadPool(int64_t m_min = 2, int64_t m_max = static_cast<int64_t>(std::thread::hardware_concurrency()));
     ~ThreadPool();
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool(const ThreadPool&&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&&) = delete;
 
-    void addTask(const std::function<void()>& task);
+    // 初始化原子
+    explicit ThreadPool(int64_t min = 2, int64_t max = std::thread::hardware_concurrency());
+
+    // 增加任务
+    void add_task(const std::function<void()>& task);
+
+    // 异步的增加
+    template <typename T, typename... Args>
+    auto add_task(T &&t, Args&&... args) -> std::future<std::result_of_t<T(Args...)>> {
+        using return_type_ = std::result_of_t<T(Args...)>;
+
+        auto tmp = std::make_shared<std::packaged_task<return_type_()>>(
+            // 必须用完美转发
+            std::bind(std::forward<T>(t), std::forward<Args>(args)...)
+        );
+
+        std::future<return_type_> res = tmp->get_future();
+        {
+            std::unique_lock locker(mutex_);
+            tasks_.emplace([tmp]() mutable {
+                try {
+                    (*tmp)();
+                }
+                catch (const std::exception& e) {
+                    std::cout << e.what() << std::endl;
+                }
+                });
+        }
+        condition_variable_.notify_one();
+        return res;
+    }
+
 
 private:
-    void worker();
-    void manager();
+    // 管理者线程的作用方案
+    void manage();
+
+    // 工作线程的作用方案
+    void work();
 
 private:
-    // main control
-    std::atomic_bool m_stop;
+    // 参数
+    std::atomic_bool is_stop_;
 
-     // size is not sure
-    std::atomic_int64_t m_minSize = 0;
-    std::atomic_int64_t m_maxSize = 0;
+    std::atomic_int64_t min_size_;
+    std::atomic_int64_t max_size_;
 
-    std::atomic_int64_t current_work_size = 0;
-    std::atomic_int64_t current_free_size = 0;
+    std::atomic_int64_t free_size_;
+    std::atomic_int64_t exit_size_;
+    std::atomic_int64_t current_size_;
 
 private:
-    // sync
-    std::mutex m_queue_mutex;
-    std::condition_variable m_condition;
+    // 对象
+    std::unique_ptr<std::thread> manager_;
+    std::unordered_map<std::thread::id, std::thread> workers_;
 
-    std::vector<std::thread> m_workers;
-    std::unique_ptr<std::thread> m_manager;
+    // 任务队列
+    std::queue<std::function<void()>> tasks_;
+    std::vector<std::thread::id> exit_task_ids_;
 
-    std::queue<std::function<void()>> m_tasks;
+    // 线程安全
+    std::mutex mutex_;
+    std::mutex mutex_ids_;
+    std::condition_variable condition_variable_;
+
 };
 
 #endif //THREADPOOL_HPP
--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -2,58 +2,126 @@
 // Created by whx on 24-10-21.
 //
 
-#include <../include/ThreadPool.hpp>
+#include "../include/ThreadPool.hpp"
 
-ThreadPool::ThreadPool(int64_t m_min, int64_t m_max) :
-    m_stop(false),
-    m_minSize(m_min),m_maxSize(m_max),
-    current_work_size(m_min),current_free_size(m_min)
+#include <chrono>
+#include <iostream>
+
+ThreadPool::ThreadPool(const int64_t min, const int64_t max) :
+    is_stop_(false),
+    min_size_(min), max_size_(max),
+    free_size_(max), current_size_(max)
 {
-    m_manager = std::make_unique<std::thread>(&ThreadPool::manager, this);
+    // 创建管理者线程
+    manager_ = std::make_unique<std::thread>(&ThreadPool::manage, this);
 
-    for(auto i = 0; i < m_min; i++) {
-        m_workers.emplace_back(&ThreadPool::worker, this);
+    // 创建工作线程
+    for (auto i = 0; i < max; i++) {
+        std::thread tmp(&ThreadPool::work, this);
+        workers_[tmp.get_id()] = std::move(tmp);
     }
-
 }
 
-void ThreadPool::addTask(const std::function<void()>& task) {
+ThreadPool::~ThreadPool() {
+    is_stop_ = true;
+    condition_variable_.notify_all();
+
+    // ReSharper disable once CppUseElementsView
+    for (auto &[fst, snd] : workers_) {
+        if(std::thread& it = snd;
+            it.joinable()) {
+            std::cout << "线程已销毁:" << std::this_thread::get_id() << std::endl;
+            it.join();
+        }
+    }
+
+    if (manager_->joinable()) {
+        manager_->join();
+    }
+}
+
+void ThreadPool::add_task(const std::function<void()>& task) {
     {
-        std::lock_guard<std::mutex> lock_guard(m_queue_mutex);
-        m_tasks.emplace(task);
+        std::lock_guard locker(mutex_);
+        tasks_.emplace(task);
     }
-    m_condition.notify_one();
+    condition_variable_.notify_one();
 }
 
+void ThreadPool::manage() {
+    while(!is_stop_.load()) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
 
-void ThreadPool::worker() {
-    while(!m_stop.load()) {
-        std::function<void()> task = nullptr;
-        {
-            std::unique_lock<std::mutex> locker(m_queue_mutex);
+        const int64_t free = free_size_.load();
+        const int64_t current = current_size_.load();
 
-            // empty
-            while (m_tasks.empty() && m_stop) {
-                m_condition.wait(locker);
+        // 销毁线程
+        if (free > current / 2 && current > min_size_) {
+            exit_size_.store(2);
+            condition_variable_.notify_all();
+
+            std::lock_guard<std::mutex> locker_for_ids(mutex_ids_);
+
+            for (const auto &id : exit_task_ids_) {
+                if(workers_.contains(id)) {
+                    std::cout << "----线程----：" <<
+                           id << "被销毁" << std::endl;
+                    workers_[id].join();
+                    workers_.erase(id);
+                }
             }
 
-            if (!m_tasks.empty()) {
-                task = std::move(m_tasks.front());
-                m_tasks.pop();
+            exit_task_ids_.clear();
+
+        }
+
+        // 增加线程
+        else if (free == 0 && current < max_size_) {
+            std::thread tmp(&ThreadPool::work, this);
+            workers_[tmp.get_id()] = std::move(tmp);
+            ++current_size_;
+            ++free_size_;
+        }
+
+    }
+}
+
+void ThreadPool::work() {
+    while(!is_stop_.load()) {
+        std::function<void()> task = nullptr;
+
+        {
+            std::unique_lock locker(mutex_);
+
+            // 任务队列空
+            while (tasks_.empty() && is_stop_) {
+                condition_variable_.wait(locker);
+
+                if (exit_size_) {
+                    --current_size_;
+                    --free_size_;
+                    --exit_size_;
+                    std::cout << "有线程退出了，ID是：" << std::this_thread::get_id() << std::endl;
+                    std::lock_guard<std::mutex> locker_for_ids(mutex_ids_);
+                    exit_task_ids_.push_back(std::this_thread::get_id());
+                    return;
+                }
+            }
+
+            if (!tasks_.empty()) {
+                std::cout << "任务取出" << std::endl;
+                task = std::move(tasks_.front());
+                tasks_.pop();
             }
         }
 
         if (task) {
-            // 此处可以进行绑定器实现有参传递
-            --current_free_size;
+            // 可以考虑一下为什么不用后置
+            --free_size_;
             task();
-            ++current_free_size;
+            ++free_size_;
         }
     }
 }
 
-void ThreadPool::manager() {
-    while(!m_stop.load()) {
-        std::this_thread::sleep_for(std::chrono::seconds(3));
-    }
-}
+
